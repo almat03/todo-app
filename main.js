dfc74b9472368614c1b5eb1b597bd46380070c9a
alem <al.ma0305@protonmail.com>
2022-07-15 00:51:49 +0200

    working also with local storage

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let form = document.getElementById("form");
 let textInput = document.getElementById("textInput");
 let msg = document.getElementById("msg");
-let data = {};
+let data = [];
 let dateInput = document.getElementById("dateInput");
 let textarea = document.getElementById("textarea");
 let tasks = document.getElementById("tasks");
@@ -18,34 +18,42 @@ let formValidation = () => {
   } else {
     msg.innerHTML = "";
     acceptData();
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click();
+    (() => {
+      add.setAttribute("data-bs-dismiss", "");
+    })();
   }
 };
 
-// function that fetch data in the input and store in data object
+// function that fetch data in the input and store in the data array of object
 let acceptData = () => {
-  data["text"] = textInput.value;
-  data["date"] = dateInput.value;
-  data["descrption"] = textarea.value;
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value,
+  });
+  // pushing data in local storage
+  localStorage.setItem("data", JSON.stringify(data));
   createTask();
-  add.setAttribute("data-bs-dismiss", "modal");
-  add.click();
-  (() => {
-    add.setAttribute("data-bs-dismiss", "");
-  })();
 };
 
 let createTask = () => {
-  tasks.innerHTML += `
-<div>
-  <span class="fw-bold">${data.text}</span>
-  <span class="small secondary">${data.date}</span>
-    <p>${data.descrption}</p>
-    <span class="options">
-      <i onClick="uploadTask(this)" class="fa-solid fa-pen-to-square"></i>
-      <i onClick="deleteTask(this)" class="fa-solid fa-trash-can"></i>
-    </span>
-</div>
-`;
+  tasks.innerHTML = "";
+  data.map((card, cards) => {
+    return (tasks.innerHTML += `
+    <div id=${cards}>
+      <span class="fw-bold">${card.text}</span>
+      <span class="small secondary">${card.date}</span>
+        <p>${card.description}</p>
+        <span class="options">
+          <i onClick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa-solid fa-pen-to-square"></i>
+          <i onClick="deleteTask(this);createTask()" class="fa-solid fa-trash-can"></i>
+        </span>
+    </div>
+    `);
+  });
+
   resetForm();
 };
 
@@ -56,7 +64,26 @@ let resetForm = () => {
 };
 
 let deleteTask = (e) => {
-  e.parentElement.parentElement.remove();
+  let selectedTask = e.parentElement.parentElement;
+  selectedTask.remove();
+  data.splice(selectedTask.id, 1);
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
 };
 
-let uploadTask = (e) => {};
+let editTask = (e) => {
+  // select a specific task
+  let selectedTask = e.parentElement.parentElement;
+  //pick up the input, the date e the textarea ( they are children of the selectedTask)
+  textInput.value = selectedTask.children[0].innerHTML;
+  dateInput.vale = selectedTask.children[1].innerHTML;
+  textarea.value = selectedTask.children[2].innerHTML;
+  //removed the task so we can update him
+  deleteTask(e);
+};
+
+(() => {
+  data = JSON.parse(localStorage.getItem("data")) || [];
+  createTask();
+  console.log(data);
+})();
